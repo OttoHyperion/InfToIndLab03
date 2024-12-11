fcbebe344a87bde2b37f5b800af236f150c7034d
Otto_Hyperion <petrov57_00@mail.ru>
2024-12-11 20:34:28 +0500

    rebith

--- src/lib.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-
-
-def reverse_array(arr: np.array):
-    arr = arr.copy()
-    arr_len = len(arr)
-    for i in range(arr_len // 2):
-        arr[i], arr[arr_len - 1 - i] = arr[arr_len - 1 - i], arr[i]
-    return arr
